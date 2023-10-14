6736843a0ade5ae258486970f94b3a360622d729
Vrund <svrund2000@yahoo.com>
2023-10-15 01:46:55 +0530

    Changes Modifies as per the AiCore Task

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,15 @@
+import random
 my_fav_fruits = ['Apple', 'Banana', 'Orange', 'Strawberry', 'Mango']
 word_list = my_fav_fruits
-print(word_list)
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter: ")
+if len(guess) == 1:
+    print("Good guess!")
+
+else:
+    print ("Oops! That is not a valid input.")
+
+
+
