b18b794a2524872b1271efa3068d35cdb137a72e
Vrund <svrund2000@yahoo.com>
2023-10-16 13:39:33 +0530

    To play the Hangman game, create an instance of the Hangman class with a word list and start guessing letters using the ask_for_input method.

---
+++ milestone_4.py
@@ -0,0 +1,34 @@
+import random
+
+class Hangman:
+    
+   def check_guess(self, guess):
+        """
+        Check if the guessed letter is in the word and update word_guessed.
+
+        Parameters:
+        guess (str): The guessed letter.
+        """
+        # Step 1: Convert the guessed letter to lower case
+        guess = guess.lower()
+
+        # Step 2: Create an if statement that checks if the guess is in the word
+        if guess in self.word:
+            # In the body of the if statement, print a message
+            print(f"Good guess! {guess} is in the word.")
+
+            # Update word_guessed by replacing underscores with the guessed letter
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+
+            # Reduce the variable num_letters by the count of guessed letter occurrences
+            self.num_letters -= self.word.count(guess)
+        else:
+            # If the guess is not in the word, reduce num_lives and provide messages
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+
+    
